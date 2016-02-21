ed66c6373f8c97fbd6bc81c38f44da4589236178
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-02-20 21:46:12 -0500

    Change

---
+++ assign05.js
@@ -0,0 +1,57 @@
+
+//call and start the express module
+var express = require('express');
+var app = express();
+
+//call and connect body parser
+var bodyParser = require('body-parser'); 
+app.use(bodyParser.json())
+
+//http://stackoverflow.com/questions/24543847/req-body-empty-on-posts
+app.use(bodyParser.urlencoded({extended: false}));
+
+//GET
+app.get('/', function(req,res){
+	
+	//message console
+	console.log("I received a GET request.");
+	
+	//res.writeHead(200, { "Content-Type": "text/plain" });
+	//res.end("hello! It's me.");
+	res.send("This was a GET.");
+	
+});
+
+//POST
+app.post('/', function(req,res){
+	
+	
+	//message console
+	console.log("I received a POST request.");
+	
+	res.send("This was a POST.");
+
+	
+	
+});
+
+//catch not found
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+
+//catch error
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+
+//listen on port
+console.log("Listening on port 3100...");
+app.listen(3100);
