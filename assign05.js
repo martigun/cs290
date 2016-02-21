70177f81976459348726ce973ddad275a026b04c
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-02-21 10:21:40 -0500

    change

--- assign05.js
+++ assign05.js
@@ -1,11 +1,16 @@
+//Gunnar Martin
+//CS290
+//Winter 2015
+//Assign05: GET and POST checker
 
 //call and start the express module
 var express = require('express');
 var app = express();
 
-//call and connect body parser
+//call and connect body parser for post requests
 var bodyParser = require('body-parser'); 
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false })); //form data from URLs
+app.use(bodyParser.json()); //json data
 
 //call and build handlebars
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
@@ -14,57 +19,75 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine','handlebars');
 
-
-//http://stackoverflow.com/questions/24543847/req-body-empty-on-posts
-app.use(bodyParser.urlencoded({extended: false}));
-
-//GET
+//GET request
 app.get('/', function(req,res){
 	
-	//message console
-	console.log("I received a GET request.");
+	//declare empty array for query string
+	var qsArray = [];
 	
-	//res.type("text/plain");
-	//res.send("This was a GET.");
+	//fill array with query params
+	for (var p in req.query){
+		
+		//fill array with objects
+		qsArray.push({'field':p,'data-value':req.query[p]})
+	}
 	
-	res.render('get.handlebars');
+	//set up object to pass and set qsArray
+	var obj = {};
+	obj.qsArray = qsArray;
+	obj.reqType = "GET";
+	res.render('assign05.handlebars',obj);
 	
 });
 
 //POST
 app.post('/', function(req,res){
 	
+	//declare empty array
+	var qsArray = [];
 	
-	//message console
-	console.log("I received a POST request.");
+	//fill array with query params
+	for (var p in req.query){
+		
+		//fill array with objects
+		qsArray.push({'field':p,'data-value':req.query[p]})
+	}
 	
-	//res.type("text/plain");
-	//res.send("This was a POST. Data was posted.");
+	//declare empty array
+	var pdArray = [];
 	
-	res.render('post.handlebars');
-
+	//fill array with body data
+	for (var p in req.body){
+		
+		//fill array with objects
+		pdArray.push({'field':p,'data-value':req.body[p]})
+	}
 	
+	//set up object to pass and set qsArray
+	var obj = {};
+	obj.qsArray = qsArray;
+	obj.pdArray = pdArray;
+	obj.reqType = "POST";
+	obj.postParams = "Posted Fields and Values";
+	res.render('assign05.handlebars',obj);
 	
 });
 
 //catch not found
 app.use(function(req,res){
   
-  res.status(404);
-  
-  //res.type('text/plain');
-  //res.send('404 - Not Found');
-  
-  res.render('post.handlebars');
-});
+	res.status(404);
 
+	res.type('text/plain');
+	res.send('404 - Not Found');
+});
 
 //catch error
 app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.type('plain/text');
-  res.status(500);
-  res.send('500 - Server Error');
+	console.error(err.stack);
+	res.type('plain/text');
+	res.status(500);
+	res.send('500 - Server Error');
 });
 
 
